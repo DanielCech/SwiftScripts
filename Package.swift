ec2b8f81ddcf8db07c8ff4c243065cf744ae6f1a
Daniel Čech <daniel.cech@gmail.com>
2019-02-05 23:35:18 +0100

    [feat]: swift scripts repo

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:4.0
+
+/**
+ *  SwiftScripts
+ *  Copyright (c) Daniel Cech 2019
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftScripts",
+    products: [
+        .library(name: "SwiftScripts", targets: ["SwiftScripts"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftScripts",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SwiftScriptsTests",
+            dependencies: ["SwiftScripts"]
+        )
+    ]
+)
