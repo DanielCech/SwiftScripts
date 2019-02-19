83b388d4b856bc860844335623b95b16a75768af
Daniel Čech <daniel.cech@gmail.com>
2019-02-19 15:20:12 +0100

    [feat]: renameEpisodes working

--- renameEpisodes/renameEpisodes.swift
+++ renameEpisodes/renameEpisodes.swift
@@ -88,11 +88,11 @@ extension String {
 }
 
 
-struct Series: Decodable {
+struct Season: Decodable {
     var episodes: [Episode]
-    var season: String
+    var season: Int
     var title: String
-    var totalSeasons: String
+    var totalSeasons: Int
 
     enum CodingKeys: String, CodingKey {
         case episodes = "Episodes"
@@ -101,16 +101,26 @@ struct Series: Decodable {
         case totalSeasons
     }
 
-    init(episodes: [Episode], season: String, title: String, totalSeasons: String) {
+    init(episodes: [Episode], season: Int, title: String, totalSeasons: Int) {
         self.episodes = episodes
         self.season = season
         self.title = title
         self.totalSeasons = totalSeasons
     }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let episodes: [Episode] = try container.decode([Episode].self, forKey: .episodes)
+        let season: Int = Int(try container.decode(String.self, forKey: .season)) ?? 0
+        let title: String = try container.decode(String.self, forKey: .title)
+        let totalSeasons: Int = Int(try container.decode(String.self, forKey: .totalSeasons)) ?? 0
+
+        self.init(episodes: episodes, season: season, title: title, totalSeasons: totalSeasons)
+    }
 }
 
 struct Episode: Decodable {
-    var episode: String
+    var episode: Int
     var title: String
 
     enum CodingKeys: String, CodingKey {
@@ -118,9 +128,16 @@ struct Episode: Decodable {
         case title = "Title"
     }
 
-    init(episode: String, title: String) {
+    init(episode: Int, title: String) {
         self.episode = episode
         self.title = title
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let episode: Int = Int(try container.decode(String.self, forKey: .episode)) ?? 0
+        let title: String = try container.decode(String.self, forKey: .title)
+        self.init(episode: episode, title: title)
     }
 }
 
@@ -138,21 +155,24 @@ func matches(for regex: String, in text: String) -> [String] {
     }
 }
 
-var seriesInfo: Series!
+var seriesInfo = [Int: Season]()
 
 extension File {
-    func renameEpisode(series: String?, episode: String?) -> Bool {
+    func renameEpisode(season: String?, episode: String?) -> Bool {
         guard
-            let unwrappedSeries = series,
-            let seriesInt = Int(unwrappedSeries),
+            let unwrappedSeason = season,
+            let seasonInt = Int(unwrappedSeason),
             let unwrappedEpisode = episode,
             let episodeInt = Int(unwrappedEpisode)
             else { return false }
 
-//        guard episodeInt <= seriesInfo.totalSeasons else { return false }
-        let episodeName = seriesInfo.episodes[episodeInt].title
-        print("rename: \(name) -> S\(seriesInt)E\(episodeInt) \(episodeName).\(self.extension ?? "")")
+        guard seasonInt <= seriesInfo.keys.count else { return false }
+        guard let season = seriesInfo[seasonInt] else { return false }
+        guard episodeInt <= season.episodes.count else { return false }
 
+        let episodeName = season.episodes[episodeInt - 1].title
+        let newName = String(format: "S%02dE%02d %@.%@", seasonInt, episodeInt, episodeName, self.extension ?? "")
+        print("  -> \(newName)")
         return true
     }
 }
@@ -165,33 +185,33 @@ func performRenames(inputDir: String) throws {
         let fileName = file.nameExcludingExtension
 
         if let descriptor = matches(for: "[sS]\\d\\d[eE]\\d\\d", in: fileName).first {
-            let seriesNumber = String(descriptor[1...2])
+            let seasonNumber = String(descriptor[1...2])
             let episodeNumber = String(descriptor[4...5])
-            if file.renameEpisode(series: seriesNumber, episode: episodeNumber) {
+            if file.renameEpisode(season: seasonNumber, episode: episodeNumber) {
                 continue
             }
         }
 
         if let descriptor = matches(for: "[sS]\\d[eE]\\d\\d", in: fileName).first {
-            let seriesNumber = String(descriptor[1...1])
+            let seasonNumber = String(descriptor[1...1])
             let episodeNumber = String(descriptor[3...4])
-            if file.renameEpisode(series: seriesNumber, episode: episodeNumber) {
+            if file.renameEpisode(season: seasonNumber, episode: episodeNumber) {
                 continue
             }
         }
 
         if let descriptor = matches(for: "[sS]\\d[eE]\\d", in: fileName).first {
-            let seriesNumber = String(descriptor[1...1])
+            let seasonNumber = String(descriptor[1...1])
             let episodeNumber = String(descriptor[3...3])
-            if file.renameEpisode(series: seriesNumber, episode: episodeNumber) {
+            if file.renameEpisode(season: seasonNumber, episode: episodeNumber) {
                 continue
             }
         }
 
         if let descriptor = matches(for: "\\d\\d[xX]\\d\\d", in: fileName).first {
-            let seriesNumber = String(descriptor[0...1])
+            let seasonNumber = String(descriptor[0...1])
             let episodeNumber = String(descriptor[3...4])
-            if file.renameEpisode(series: seriesNumber, episode: episodeNumber) {
+            if file.renameEpisode(season: seasonNumber, episode: episodeNumber) {
                 continue
             }
         }
@@ -200,6 +220,24 @@ func performRenames(inputDir: String) throws {
     }
 }
 
+func renameSeries(name seriesName: String, season: Int, inputDir: String) {
+    print("🔦 Downloading info about season \(season)")
+
+    let url = "http://www.omdbapi.com/?apikey=d1b21f08&type=series&t=\(seriesName)&Season=\(season)"
+
+    Alamofire.request(url).response { response in
+        if let data = response.data {
+            let seasonInfo = try! JSONDecoder().decode(Season.self, from: data)
+
+            seriesInfo[season] = seasonInfo
+            try? performRenames(inputDir: inputDir)
+
+            if season < seasonInfo.totalSeasons {
+                renameSeries(name: seriesName, season: season + 1, inputDir: inputDir)
+            }
+        }
+    }
+}
 
 let moderator = Moderator(description: "Rename seriers episodes using names from OMDb. Script will ask for confirmation.")
 
@@ -219,15 +257,8 @@ do {
     print("⌚️ Processing")
 
     let seriesName = unwrappedName.replacingOccurrences(of: " ", with: "+")
-    let url = "http://www.omdbapi.com/?apikey=d1b21f08&type=series&t=\(seriesName)&Season=1"
 
-
-    Alamofire.request(url).response { response in
-        if let data = response.data {
-            seriesInfo = try! JSONDecoder().decode(Series.self, from: data)
-            try! performRenames(inputDir: unwrappedInputDir)
-        }
-    }
+    renameSeries(name: seriesName, season: 1, inputDir: unwrappedInputDir)
 
     RunLoop.main.run(until: Date(timeIntervalSinceNow: 2))
 
