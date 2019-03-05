143770fca2af8542ecd225f1ac4fa02d104578f3
Daniel Čech <daniel.cech@gmail.com>
2019-03-05 16:13:26 +0100

    [feat]: versionIcon without dependencies

--- versionIcon/versionIcon.swift
+++ versionIcon/versionIcon.swift
@@ -1,25 +1,155 @@
 import Foundation
+import AppKit
 import Files
 import SwiftShell
 import Moderator
 import ScriptToolkit
 
+// Based on: https://gist.github.com/MaciejGad/11d8469b218817290ee77012edb46608
+
+extension NSImage {
+
+    /// Returns the height of the current image.
+    var height: CGFloat {
+        return self.size.height
+    }
+
+    /// Returns the width of the current image.
+    var width: CGFloat {
+        return self.size.width
+    }
+
+    /// Returns a png representation of the current image.
+    var PNGRepresentation: Data? {
+        if let tiff = self.tiffRepresentation, let tiffData = NSBitmapImageRep(data: tiff) {
+            return tiffData.representation(using: .png, properties: [:])
+        }
+
+        return nil
+    }
+
+    ///  Copies the current image and resizes it to the given size.
+    ///
+    ///  - parameter size: The size of the new image.
+    ///
+    ///  - returns: The resized copy of the given image.
+    func copy(size: NSSize) throws -> NSImage {
+        // Create a new rect with given width and height
+        let frame = NSMakeRect(0, 0, size.width, size.height)
+
+        // Get the best representation for the given size.
+        guard let rep = self.bestRepresentation(for: frame, context: nil, hints: nil) else {
+            throw ScriptError.generalError(message: "Unable to resize image")
+        }
+
+        // Create an empty image with the given size.
+        let img = NSImage(size: size)
+
+        // Set the drawing context and make sure to remove the focus before returning.
+        img.lockFocus()
+        defer { img.unlockFocus() }
+
+        // Draw the new image
+        if rep.draw(in: frame) {
+            return img
+        }
+
+        // Return nil in case something went wrong.
+        throw ScriptError.generalError(message: "Unable to resize image")
+    }
+
+    ///  Saves the PNG representation of the current image to the HD.
+    ///
+    /// - parameter url: The location url to which to write the png file.
+    func savePNGRepresentationToURL(url: URL) throws {
+        if let png = self.PNGRepresentation {
+            try png.write(to: url, options: .atomicWrite)
+        }
+    }
+
+    func combine(withImage image: NSImage) throws -> NSImage {
+        guard
+            let firstImageData = image.tiffRepresentation,
+            let firstImage = CIImage(data: firstImageData),
+            let secondImageData = tiffRepresentation,
+            let secondImage = CIImage(data: secondImageData)
+        else {
+            throw ScriptError.generalError(message: "Image processing error")
+        }
+
+        let filter = CIFilter(name: "CISourceOverCompositing")!
+        filter.setDefaults()
+
+        filter.setValue(firstImage, forKey: "inputImage")
+        filter.setValue(secondImage, forKey: "inputBackgroundImage")
+
+        let resultImage = filter.outputImage
+
+        let rep = NSCIImageRep(ciImage: resultImage!)
+        let finalResult = NSImage(size: rep.size)
+        finalResult.addRepresentation(rep)
+
+        return finalResult
+    }
+
+    func image(withText text: String, attributes: [NSAttributedString.Key: Any]) -> NSImage {
+        let image = self
+        let text = text as NSString
+        let options: NSString.DrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
+
+        let textSize = text.boundingRect(with: image.size, options: options, attributes: attributes).size
+
+        let x = (image.size.width - textSize.width) / 2
+        let y = (image.size.height - textSize.height) / 2
+        let point = NSMakePoint(x, y * 0.2)
+
+        image.lockFocus()
+        text.draw(at: point, withAttributes: attributes)
+        image.unlockFocus()
+
+        return image
+    }
+
+    func annotate(text: String, size: CGFloat, fill: NSColor, stroke: NSColor) -> NSImage {
+
+        // Solid color text
+        let fillText = image(
+            withText: text,
+            attributes: [
+                .foregroundColor: fill,
+                .font: NSFont(name: "Impact", size: size)!
+            ])
+
+        // Add strokes
+        return fillText.image(
+            withText: text,
+            attributes: [
+                .foregroundColor: fill,
+                .strokeColor: stroke,
+                .strokeWidth: 1,
+                .font: NSFont(name: "Impact", size: size)!
+            ])
+    }
+}
+
+
+
 func generateIcon(_ icon: String, size: CGSize, ribbon: String, title: String, fillColor: String, strokeColor: String, original: Bool, scriptPath: String) throws {
     print(icon)
     guard
         let sourceRootPath = main.env["SRCROOT"],
-        let buildPath = main.env["BUILT_PRODUCTS_DIR"],
-        let unlocalizedResourcesPath = main.env["UNLOCALIZED_RESOURCES_FOLDER_PATH"],
         let projectDir = main.env["PROJECT_DIR"],
         let infoPlistFile = main.env["INFOPLIST_FILE"]
         else {
             print("Missing environment variables")
-            throw ScriptError.moreInfoNeeded(message: "Missing required environment variables SRCROOT, BUILT_PRODUCTS_DIR, UNLOCALIZED_RESOURCES_FOLDER_PATH, PROJECT_DIR, INFOPLIST_FILE")
+            throw ScriptError.moreInfoNeeded(message: "Missing required environment variables: SRCROOT, PROJECT_DIR, INFOPLIST_FILE")
     }
 
+//    let sourceRootPath = "/Users/dan/Documents/[Development]/[Projects]/RoboticArmApp"
+//    let projectDir = "/Users/dan/Documents/[Development]/[Projects]/RoboticArmApp"
+//    let infoPlistFile = "Arm/Info.plist"
+
     print("  sourceRootPath: \(sourceRootPath)")
-    print("  buildPath: \(buildPath)")
-    print("  unlocalizedResourcesPath: \(unlocalizedResourcesPath)")
     print("  projectDir: \(projectDir)")
     print("  infoPlistFile: \(infoPlistFile)")
 
@@ -49,8 +179,7 @@ func generateIcon(_ icon: String, size: CGSize, ribbon: String, title: String, f
         try baseImageFile.copy(to: appIconFolder)
     }
     else {
-        let tempPath = "/tmp/.versionIcon"
-        try FileSystem().createFolderIfNeeded(at: tempPath)
+        let newSize = CGSize(width: size.width / 2, height: size.height / 2)
 
         let ribbonsDirPath = scriptPath.appendingPathComponent(path: "Ribbons")
         let ribbonPath = ribbonsDirPath.appendingPathComponent(path: ribbon)
@@ -59,23 +188,24 @@ func generateIcon(_ icon: String, size: CGSize, ribbon: String, title: String, f
         let titlePath = titlesDirPath.appendingPathComponent(path: title)
 
         print("  Resizing ribbon")
-        let resizedRibbonPath = tempPath.appendingPathComponent(path: "resizedRibbon.png")
-        try File(path: ribbonPath).resizeImage(newName: resizedRibbonPath, size: size)
+        guard let ribbonImage = NSImage(contentsOfFile: ribbonPath) else { throw ScriptError.generalError(message: "Invalid image file") }
+        let resizedRibbonImage = try ribbonImage.copy(size: newSize)
 
         print("  Resizing title")
-        let resizedTitlePath = tempPath.appendingPathComponent(path: "resizedTitle.png")
-        try File(path: titlePath).resizeImage(newName: resizedTitlePath, size: size)
+        guard let titleImage = NSImage(contentsOfFile: titlePath) else { throw ScriptError.generalError(message: "Invalid image file") }
+        let resizedTitleImage = try titleImage.copy(size: newSize)
+
+        let iconImageData = try Data(contentsOf: URL(fileURLWithPath: baseImageFile.path))
+        guard let iconImage = NSImage(data: iconImageData) else { throw ScriptError.generalError(message: "Invalid image file") }
+
+        let combinedImage = try iconImage.combine(withImage: resizedRibbonImage).combine(withImage: resizedTitleImage)
 
         print("  Annotating")
-        let annotatedFilePath = tempPath.appendingPathComponent(path: "annotated.png")
-        run(ScriptToolkit.convertPath, baseImageFile.path, "-fill", fillColor, "-stroke", strokeColor, "-font", "Impact", "-pointsize", String(Int(size.width * 0.15)), "-gravity", "south", "-annotate", "0", version, annotatedFilePath)
+        let resultImage = combinedImage.annotate(text: version, size: size.width * 0.2, fill: .white, stroke: .black)
 
-        print("  Annotating with ribbon")
-        let annotatedRibbonFilePath = tempPath.appendingPathComponent(path: "annotatedRibbon.png")
-        run(ScriptToolkit.compositePath, resizedRibbonPath, annotatedFilePath, annotatedRibbonFilePath)
+        let resizedIcon = try resultImage.copy(size: newSize)
 
-        print("  Annotating with ribbon and title")
-        run(ScriptToolkit.compositePath, resizedTitlePath, annotatedRibbonFilePath, targetPath)
+        try resizedIcon.savePNGRepresentationToURL(url: URL(fileURLWithPath: targetPath))
     }
 }
 
@@ -97,6 +227,7 @@ let iPad = moderator.add(.option("ipad", description: "Generate iPad icons"))
 let original = moderator.add(.option("original", description: "Use original icon with no modifications (for production)"))
 
 do {
+    // try moderator.parse(["--ribbon", "Red.png", "--title", "Devel.png", "--script", "/Users/dan/Documents/[Development]/[Projects]/SwiftScripts/versionIcon", "--iphone"])
     try moderator.parse()
     guard let unwrappedRibbon = ribbon.value, let unwrappedTitle = title.value, let unwrappedScriptPath = scriptPath.value else {
         print(moderator.usagetext)
