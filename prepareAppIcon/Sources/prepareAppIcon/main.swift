a8e85746692d8c1d90d8b6fc675c146a4b6f6d61
Daniel Cech <daniel.cech@strv.com>
2020-05-20 12:15:06 +0200

    [feat]: prepareAppIcon fix, removed ImageMagick dependency

--- prepareAppIcon/Sources/prepareAppIcon/main.swift
+++ prepareAppIcon/Sources/prepareAppIcon/main.swift
@@ -3,11 +3,24 @@ import Files
 import SwiftShell
 import Moderator
 import ScriptToolkit
+import AppKit
+
+extension File {
+    func resizeImage(newName: String, size: CGSize) throws {
+        let image: NSImage? = NSImage(contentsOfFile: self.path)
+        let newImage = image.map { try? $0.copy(size: size) } ?? nil
+        if let unwrappedNewImage = newImage {
+            
+            try unwrappedNewImage.savePNGRepresentationToURL(url: URL(fileURLWithPath: newName))
+        }
+    }
+}
 
 let moderator = Moderator(description: "prepareAppIcon - Prepare all required resolutions of iOS app icon")
 moderator.usageFormText = "prepareappicon <params>"
 
-let inFile = moderator.add(Argument<String?>.singleArgument(name: "input"))
+let inFile = moderator.add(Argument<String?>
+.optionWithValue("input", name: "Input file", description: "PNG file preferably with 1024x1024 resolution"))
 
 do {
     try moderator.parse()
@@ -19,26 +32,44 @@ do {
     print("⚙️ Processing icons")
     let inputFile = try File(path: unwrappedInFile)
 
-    try inputFile.resizeImage(newName: "iPhoneNotification-20x20@2x.png", size: CGSize(width: 40, height: 40))
-    try inputFile.resizeImage(newName: "iPhoneNotification-20x20@3x.png", size: CGSize(width: 60, height: 60))
-    try inputFile.resizeImage(newName: "iPhoneSpotlightSettings-29x29@2x.png", size: CGSize(width: 58, height: 58))
-    try inputFile.resizeImage(newName: "iPhoneSpotlightSettings-29x29@3x.png", size: CGSize(width: 87, height: 87))
-    try inputFile.resizeImage(newName: "iPhoneSpotlight-40x40@2x.png", size: CGSize(width: 80, height: 80))
-    try inputFile.resizeImage(newName: "iPhoneSpotlight-40x40@3x.png", size: CGSize(width: 120, height: 120))
-    try inputFile.resizeImage(newName: "iPhoneApp-60x60@2x.png", size: CGSize(width: 120, height: 120))
-    try inputFile.resizeImage(newName: "iPhoneApp-60x60@3x.png", size: CGSize(width: 180, height: 180))
-    try inputFile.resizeImage(newName: "iPadNotification-20x20.png", size: CGSize(width: 20, height: 20))
-    try inputFile.resizeImage(newName: "iPadNotification-20x20@2x.png", size: CGSize(width: 40, height: 40))
-    try inputFile.resizeImage(newName: "iPadSettings-29x29.png", size: CGSize(width: 29, height: 29))
-    try inputFile.resizeImage(newName: "iPadSettings-29x29@2x.png", size: CGSize(width: 58, height: 58))
-    try inputFile.resizeImage(newName: "iPadSpotlight-40x40.png", size: CGSize(width: 40, height: 40))
-    try inputFile.resizeImage(newName: "iPadSpotlight-40x40@2x.png", size: CGSize(width: 80, height: 80))
-    try inputFile.resizeImage(newName: "iPadApp-76x76.png", size: CGSize(width: 76, height: 76))
-    try inputFile.resizeImage(newName: "iPadProApp-83,5x83,5@2x.png", size: CGSize(width: 167, height: 167))
-    try inputFile.resizeImage(newName: "iPhoneDocument-22x29.png", size: CGSize(width: 22, height: 29))
-    try inputFile.resizeImage(newName: "iPhoneDocument-22x29@2x.png", size: CGSize(width: 44, height: 58))
-    try inputFile.resizeImage(newName: "iPadDocument-64x64.png", size: CGSize(width: 64, height: 64))
-    try inputFile.resizeImage(newName: "iPadDocument-320x320.png", size: CGSize(width: 320, height: 230))
+    // App list in iTunes
+    try inputFile.resizeImage(newName: "iTunesArtwork.png", size: CGSize(width: 512, height: 512))
+    
+    // App list in iTunes for devices with retina display
+    try inputFile.resizeImage(newName: "iTunesArtwork@2x.png", size: CGSize(width: 1024, height: 1024))
+    
+    // Home screen on iPhone/iPod Touch with retina display
+    try inputFile.resizeImage(newName: "Icon-60@2x.png", size: CGSize(width: 120, height: 120))
+    
+    // Home screen on iPhone with retina HD display
+    try inputFile.resizeImage(newName: "Icon-60@3x.png", size: CGSize(width: 180, height: 180))
+    
+    // Home screen on iPad
+    try inputFile.resizeImage(newName: "Icon-76.png", size: CGSize(width: 76, height: 76))
+    
+    // Home screen on iPad with retina display
+    try inputFile.resizeImage(newName: "Icon-76@2x.png", size: CGSize(width: 152, height: 152))
+    
+    // Home screen on iPad Pro
+    try inputFile.resizeImage(newName: "Icon-83.5@2x.png", size: CGSize(width: 167, height: 167))
+    
+    // Spotlight
+    try inputFile.resizeImage(newName: "Icon-Small-40.png", size: CGSize(width: 40, height: 40))
+    
+    // Spotlight on devices with retina display
+    try inputFile.resizeImage(newName: "Icon-Small-40@2x.png", size: CGSize(width: 80, height: 80))
+    
+    // Spotlight on devices with retina HD display
+    try inputFile.resizeImage(newName: "Icon-Small-40@3x.png", size: CGSize(width: 120, height: 120))
+    
+    // Settings
+    try inputFile.resizeImage(newName: "Icon-Small.png", size: CGSize(width: 29, height: 29))
+    
+    // Settings on devices with retina display
+    try inputFile.resizeImage(newName: "Icon-Small@2x.png", size: CGSize(width: 58, height: 58))
+    
+    // Settings on devices with retina HD display
+    try inputFile.resizeImage(newName: "Icon-Small@3x.png", size: CGSize(width: 87, height: 87))
 
     print("✅ Done")
 }
